e581f0e001935e78ee2da70adc6213219bcf6073
RisPram <pram.73ris@gmail.com>
2024-09-14 23:48:06 +0530

    addition of error boundary component

--- lib/LandingPage.jsx
+++ lib/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Wrapper from './ReusableComponents/Wrapper'
+import ErrorBoundary from './ReusableComponents/ErrorBoundary'
 
 const LandingPage = () => {
   return (
-    <Wrapper>
+    <ErrorBoundary>    <Wrapper>
     <section className="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere sint culpa facilis, nobis est officia amet beatae cum dolore fugiat animi vero aperiam. Saepe similique in accusamus iusto consequatur voluptatem.
     
     Distinctio, explicabo! Velit laboriosam veritatis, reiciendis, aut optio odit dignissimos, voluptatem a saepe nemo obcaecati adipisci at dolores iste cupiditate? Qui vitae laudantium nisi nostrum, ad ea in eaque impedit.
@@ -13,6 +14,8 @@ const LandingPage = () => {
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error atque asperiores temporibus vero suscipit ex molestiae laboriosam enim ipsa. Dolorem quae officiis ducimus nisi quisquam deserunt. Earum dolorum expedita quis veritatis doloribus accusantium ut, maxime quam voluptatibus voluptas rem, beatae quae corrupti quo pariatur error ad incidunt. Hic sed totam error adipisci sint cumque sunt illum? Quam numquam distinctio et odit nesciunt, corporis repudiandae architecto sint id maiores nulla aliquam quae, minus alias quos quisquam. Dolore voluptatibus ex placeat ipsam quo quisquam impedit sunt fugit ratione quis? Neque et iure, tenetur minus ipsum facilis doloribus praesentium? Architecto veniam saepe quaerat animi dolore magni distinctio atque placeat fuga consequuntur, illo molestias corrupti exercitationem, ipsum accusantium aut? Nulla, inventore incidunt reprehenderit numquam non consequatur laborum reiciendis, pariatur vel, labore sunt facilis harum enim nihil velit. Quia aliquam enim delectus alias, laboriosam exercitationem repellendus sint eveniet quisquam! A ut commodi inventore aliquid dignissimos enim iure repudiandae? Distinctio voluptatibus ea accusantium voluptas eius, facere illum nemo exercitationem repellat laboriosam id veritatis vel et incidunt magni commodi? Molestiae, at numquam. Ex, dignissimos sapiente. Non expedita inventore eos ipsa sit aliquid itaque totam tempore! Impedit, maxime consequatur. Rem minus voluptate modi in ullam mollitia necessitatibus illo.
     </section>
     </Wrapper>
+    </ErrorBoundary>
+
   )
 }
 
---
+++ lib/ReusableComponents/ErrorBoundary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorBoundary = () => {
+  return (
+   <section>
+    error boundary
+   </section>
+  )
+}
+
+export default ErrorBoundary
