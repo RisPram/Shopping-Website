e1412f97cfbd4a16e5682aeaf021749b250dba90
RisPram <pram.73ris@gmail.com>
2024-09-15 23:47:38 +0530

    working on ui

--- lib/LandingPage.jsx
+++ lib/LandingPage.jsx
@@ -5,13 +5,10 @@ import ErrorBoundary from './ReusableComponents/ErrorBoundary'
 const LandingPage = () => {
   return (
     <ErrorBoundary>    <Wrapper>
-    <section className="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere sint culpa facilis, nobis est officia amet beatae cum dolore fugiat animi vero aperiam. Saepe similique in accusamus iusto consequatur voluptatem.
+    <section className="">
+        Coming soon!!
     
-    Distinctio, explicabo! Velit laboriosam veritatis, reiciendis, aut optio odit dignissimos, voluptatem a saepe nemo obcaecati adipisci at dolores iste cupiditate? Qui vitae laudantium nisi nostrum, ad ea in eaque impedit.
-    Modi voluptates laudantium nostrum sed culpa debitis amet labore id fuga, voluptate voluptatibus dignissimos consequuntur, dolorum hic iusto velit nisi accusamus. Molestias, placeat iusto maxime commodi itaque quo magnam aliquam!
-    Magni pariatur distinctio tempore veritatis atque mollitia culpa provident aperiam laborum vero, iure officiis, fugit delectus. Magni esse tempore similique quod ullam sit quis, laudantium enim et fugit corrupti voluptatum?
-    Deleniti doloremque sint quo necessitatibus fugit quas placeat, quisquam inventore exercitationem mollitia id et autem beatae porro eligendi totam commodi aut qui quibusdam itia ipsum voluptatem quibusdam. Consectetur, ex?
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error atque asperiores temporibus vero suscipit ex molestiae laboriosam enim ipsa. Dolorem quae officiis ducimus nisi quisquam deserunt. Earum dolorum expedita quis veritatis doloribus accusantium ut, maxime quam voluptatibus voluptas rem, beatae quae corrupti quo pariatur error ad incidunt. Hic sed totam error adipisci sint cumque sunt illum? Quam numquam distinctio et odit nesciunt, corporis repudiandae architecto sint id maiores nulla aliquam quae, minus alias quos quisquam. Dolore voluptatibus ex placeat ipsam quo quisquam impedit sunt fugit ratione quis? Neque et iure, tenetur minus ipsum facilis doloribus praesentium? Architecto veniam saepe quaerat animi dolore magni distinctio atque placeat fuga consequuntur, illo molestias corrupti exercitationem, ipsum accusantium aut? Nulla, inventore incidunt reprehenderit numquam non consequatur laborum reiciendis, pariatur vel, labore sunt facilis harum enim nihil velit. Quia aliquam enim delectus alias, laboriosam exercitationem repellendus sint eveniet quisquam! A ut commodi inventore aliquid dignissimos enim iure repudiandae? Distinctio voluptatibus ea accusantium voluptas eius, facere illum nemo exercitationem repellat laboriosam id veritatis vel et incidunt magni commodi? Molestiae, at numquam. Ex, dignissimos sapiente. Non expedita inventore eos ipsa sit aliquid itaque totam tempore! Impedit, maxime consequatur. Rem minus voluptate modi in ullam mollitia necessitatibus illo.
+    
     </section>
     </Wrapper>
     </ErrorBoundary>
